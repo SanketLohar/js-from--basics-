69822d3b6cfdfda190b545e4d609ea69bfd39b0e
SanketLohar <loharsanket105@gmail.com>
2024-12-18 15:14:19 +0000

    updated

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,21 @@
+let accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+let accountCountry;
+
+accountId = 2 
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+accountCountry = "Beng"
+accountState="mh"
+console.log(accountId);
+console.log(accountState);
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState,accountCountry])
